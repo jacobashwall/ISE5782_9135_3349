1b26da03d6938a1c50b720ccf26e43081c6e954c
Yonatan Dahary <Yonatan Dahary@DESKTOP-98JNH09>
2022-03-30 19:40:45 +0300

    Targil 5

---
+++ src/elements/AmbientLight.java
@@ -0,0 +1,39 @@
+package elements;
+
+import primitives.*;
+
+/**
+ * Class that represents an ambient light.
+ * Ambient light is an omni-directional, fixed intensity and fixed color type of light.
+ */
+public class AmbientLight {
+
+    //private field
+    private final Color intensity;
+
+    /**
+     * Constructor that takes a Color object and an attenuation coefficient (Double3) and return
+     * the color object scaled by the attenuation coefficient.
+     * @param color Color of the ambient light
+     * @param double3 Attenuation coefficient
+     */
+    public AmbientLight(Color color, Double3 double3){
+        this.intensity = color;
+        this.intensity.scale(double3);
+    }
+
+    /**
+     * Default constructor, set the intensity field to Color.BLACK
+     */
+    public AmbientLight(){
+        this.intensity = Color.BLACK;
+    }
+
+    /**
+     * Intensity field getter
+     * @return intensity (Color)
+     */
+    public Color getIntensity(){
+        return this.intensity;
+    }
+}
--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,5 +1,8 @@
 package primitives;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * A ray in the cartesian system
  */
@@ -51,7 +54,38 @@ public class Ray {
         return this.p0.equals(other.p0) && this.dir.equals(other.dir);
     }
 
+    /**
+     * Returns the point on the end of th ray scaled by given parameter
+     * @param t Scale factor
+     * @return Point at the end of the scaled ray
+     */
     public Point getPoint(double t) {
         return p0.add(dir.scale(t));
+    }
+
+    /**
+     * Method for finding the closest point to the head of the ray in a given list of points
+     * @param lst List of points
+     * @return Closest point to the head of the ray
+     */
+    public Point findClosestPoint(List<Point> lst) {
+        //In case of empty list return null
+        if(lst.isEmpty()){
+            return null;
+        }
+
+        Point p = lst.get(0);
+        double d = lst.get(0).distance(this.getP0());
+        //Iterating through the list. Once we find smaller distance
+        //than we have we replace the values.
+        //This goes on until the end of the list.
+        for (Point pnt : lst) {
+            if (d > this.getP0().distance(pnt)) {
+                d = this.getP0().distance(pnt);
+                p = pnt;
+            }
+        }
+
+        return p;
     }
 }

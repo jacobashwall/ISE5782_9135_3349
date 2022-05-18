97e8644141d72b840aeab12020d20e59f66aff12
yakov <yakov@DESKTOP-RCEREB8>
2022-05-18 16:44:12 +0300

    bathroom

--- unittests/sampling/bathroom.java
+++ unittests/sampling/bathroom.java
@@ -22,13 +22,24 @@ public class bathroom {
      */
     @Test
     public void createPicture() {
-        //settings
+
+        //region settings
         Scene scene = new Scene("Test scene");
         Camera camera = new Camera(new Point(0, 300, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setVPSize(2000, 2000).setVPDistance(1500);
 
+        //colored tiles
+        Color[] colorPallet = {
+                new Color(152, 251, 152),//green
+                new Color(150, 150, 150),//grey
+                new Color(240, 230, 140),//yellow
+                new Color(205, 92, 92),//red
+                new Color(72, 61, 139)//blue
+        };
+        //endregion
 
-        //front wall back
+        //region front wall
+        // back
         Polygon frontWall = new Polygon(new Point(-500, 0, 0), new Point(500, 0, 0), new Point(500, 500, 0), new Point(-500, 500, 0));
         frontWall.setMaterial(new Material());
         frontWall.setEmission(new Color(122, 122, 122));
@@ -46,80 +57,154 @@ public class bathroom {
             }
         }
 
-        //front wall colored tiles
-        Color[] colorPallete = {
-                new Color(152, 251, 152),//green
-                new Color(150, 150, 150),//grey
-                new Color(240, 230, 140),//yellow
-                new Color(205, 92, 92),//red
-                new Color(72, 61, 139)//blue
-        };
-
+        //colored tiles
         tileHeight = 50;
         tileWidth = 20;
         gap = 1;
         for (int j = 0; j < 50; j++) {
-            Polygon polygon = new Polygon(new Point(-500 + j * tileWidth, 225, 2), new Point(-500 + tileWidth + j * tileWidth - gap, 225, 2), new Point(-500 + tileWidth + j * tileWidth - gap, 225 + tileHeight, 2), new Point(-500 + j * tileWidth, 225 + tileHeight, 2));
-            polygon.setEmission(colorPallete[j % 5]).setMaterial(new Material().setKs(0).setKd(0.5));
+            Polygon polygon = new Polygon(new Point(-500 + j * tileWidth, 225, 1.1), new Point(-500 + tileWidth + j * tileWidth - gap, 225, 1.1), new Point(-500 + tileWidth + j * tileWidth - gap, 225 + tileHeight, 1.1), new Point(-500 + j * tileWidth, 225 + tileHeight, 1.1));
+            polygon.setEmission(colorPallet[j % 5]).setMaterial(new Material().setKs(0).setKd(0.5));
             scene.geometries.add(polygon);
         }
+        //endregion
 
+        //region lighting
         //front wall light
-        //scene.lights.add(new PointLight(new Color(255,255,255),new Point(125,450,5)));
+        scene.lights.add(new SpotLight(new Color(200,200,200), new Point(125,450,10),new Vector(0,-1,1)).setNarrowBeam(3));
+        //scene.lights.add(new PointLight(new Color(200,200,200),new Point(125,450,15)));
+        //endregion
 
-        //mirror back
-        Polygon mirrorBack = new Polygon(new Point(-200, 275, 2), new Point(450, 275, 2), new Point(450, 425, 2), new Point(-200, 425, 2));
+        //region mirror
+        // back
+        Polygon mirrorBack = new Polygon(new Point(-200, 275, 1.1), new Point(450, 275, 1.1), new Point(450, 425, 1.1), new Point(-200, 425, 1.1));
         mirrorBack.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(mirrorBack);
 
         //mirror
         gap = 10;
-        Polygon mirror = new Polygon(new Point(-200 + gap, 275 + gap, 3), new Point(450 - gap, 275 + gap, 3), new Point(450 - gap, 425 - gap, 3), new Point(-200 + gap, 425 - gap, 3));
+        Polygon mirror = new Polygon(new Point(-200 + gap, 275 + gap, 1.2), new Point(450 - gap, 275 + gap, 1.2), new Point(450 - gap, 425 - gap, 1.2), new Point(-200 + gap, 425 - gap, 1.2));
         mirror.setEmission(new Color(0, 0, 0)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(mirror);
+        //endregion
 
-        //marble //note- need to add sink!
-        Polygon marbleTopLeft = new Polygon(new Point(-225, 200, 3), new Point(500, 200, 3), new Point(500, 200, 100), new Point(-225, 200, 100));
+        //region marble
+        Polygon marbleTopLeft = new Polygon(new Point(-225, 200, 3), new Point(50, 200, 3), new Point(50, 200, 100), new Point(-225, 200, 100));
         marbleTopLeft.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(marbleTopLeft);
+
+        Polygon marbleTopRight = new Polygon(new Point(225, 200, 3), new Point(500, 200, 3), new Point(500, 200, 100), new Point(225, 200, 100));
+        marbleTopRight.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleTopRight);
 
         Polygon marbleFront = new Polygon(new Point(500, 200, 100), new Point(-225, 200, 100), new Point(-225, 175, 100), new Point(500, 175, 100));
         marbleFront.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(marbleFront);
 
-        Polygon marbleBottomLeft = new Polygon(new Point(-225, 175, 3), new Point(500, 175, 3), new Point(500, 175, 100), new Point(-225, 175, 100));
+        Polygon marbleBottomLeft = new Polygon(new Point(-225, 175, 3), new Point(50, 175, 3), new Point(50, 175, 100), new Point(-225, 175, 100));
         marbleBottomLeft.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(marbleBottomLeft);
 
-        Polygon marbleLeft = new Polygon(new Point(-225, 200, 0), new Point(-225, 200, 100), new Point(-225, 175, 100), new Point(-225, 175, 0));
+        Polygon marbleBottomRight = new Polygon(new Point(225, 175, 3), new Point(500, 175, 3), new Point(500, 175, 100), new Point(225, 175, 100));
+        marbleBottomRight.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleBottomRight);
+
+        Polygon marbleTopClose = new Polygon(new Point(50, 200, 100), new Point(225, 200, 100), new Point(225, 200, 80), new Point(50, 200, 80));
+        marbleTopClose.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleTopClose);
+
+        Polygon marbleTopDistant = new Polygon(new Point(50, 200, 20), new Point(225, 200, 20), new Point(225, 200, 0), new Point(50, 200, 0));
+        marbleTopDistant.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleTopDistant);
+
+        Polygon marbleBottomClose = new Polygon(new Point(50, 175, 100), new Point(225, 175, 100), new Point(225, 175, 80), new Point(50, 175, 80));
+        marbleBottomClose.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleBottomClose);
+
+        Polygon marbleBottomDistant = new Polygon(new Point(50, 175, 20), new Point(225, 175, 20), new Point(225, 175, 0), new Point(50, 175, 0));
+        marbleBottomDistant.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleBottomDistant);
+
+        Polygon marbleLeft = new Polygon(new Point(-225, 200, 0), new Point(-225, 175, 0), new Point(-225, 175, 100), new Point(-225, 200, 100));
         marbleLeft.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(marbleLeft);
 
-        //closets
+        Polygon marbleHeightDistant = new Polygon(new Point(50, 175, 20), new Point(50, 200, 20), new Point(225, 200, 20), new Point(225, 175, 20));
+        marbleHeightDistant.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleHeightDistant);
+
+        Polygon marbleHeightRight = new Polygon(new Point(225, 175, 20), new Point(225, 200, 20), new Point(225, 200, 80), new Point(225, 175, 80));
+        marbleHeightRight.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleHeightRight);
+
+        Polygon marbleHeightLeft = new Polygon(new Point(50, 175, 20), new Point(50, 200, 20), new Point(50, 200, 80), new Point(50, 175, 80));
+        marbleHeightLeft.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleHeightLeft);
+
+        Polygon marbleHeightClose = new Polygon(new Point(50, 175, 80), new Point(50, 200, 80), new Point(225, 200, 80), new Point(225, 175, 80));
+        marbleHeightClose.setEmission(new Color(122, 122, 122)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(marbleHeightClose);
+        //endregion
+
+        //region sink
+        int depth = 60;
+
+        Polygon sinkDistant = new Polygon(new Point(50, 175, 20), new Point(120, 175 - depth, 40), new Point(155, 175 - depth, 40), new Point(225, 175, 20));
+        sinkDistant.setEmission(new Color(225, 225, 225)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(sinkDistant);
+
+        Polygon sinkRight = new Polygon(new Point(225, 175, 20), new Point(155, 175 - depth, 40), new Point(155, 175 - depth, 60), new Point(225, 175, 80));
+        sinkRight.setEmission(new Color(225, 225, 225)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(sinkRight);
+
+        Polygon sinkLeft = new Polygon(new Point(50, 175, 20), new Point(120, 175 - depth, 40), new Point(120, 175 - depth, 60), new Point(50, 175, 80));
+        sinkLeft.setEmission(new Color(225, 225, 225)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(sinkLeft);
+
+        Polygon sinkClose = new Polygon(new Point(50, 175, 80), new Point(120, 175 - depth, 60), new Point(155, 175 - depth, 60), new Point(225, 175, 80));
+        sinkClose.setEmission(new Color(225, 225, 225)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(sinkClose);
+
+        Polygon sinkBottom = new Polygon(new Point(120, 175 - depth, 60), new Point(155, 175 - depth, 60), new Point(155, 175 - depth, 40), new Point(120, 175 - depth, 40));
+        sinkBottom.setEmission(new Color(225, 225, 225)).setMaterial(new Material().setKs(0).setKd(0.5));
+        scene.geometries.add(sinkBottom);
+        //endregion
+
+        //region closets
         Polygon closetLeft = new Polygon(new Point(-225, 0, 100), new Point(-225, 0, 0), new Point(-225, tileHeight, 0), new Point(-225, tileHeight, 100));
         closetLeft.setEmission(new Color(100, 100, 100)).setMaterial(new Material().setKs(0).setKd(0.5));
         scene.geometries.add(closetLeft);
         tileHeight = 175;
         tileWidth = 180;
-        gap = 5;
+        int fixLeft;
+        int fixRight;
         for (int j = 0; j < 4; j++) {
+            fixLeft = 0;
+            fixRight = 0;
+            gap = 5;
+            if (j == 1) {
+                fixRight = 80;
+                gap=2;
+            }
+            if (j == 2) {
+                fixLeft = 80;
+            }
             Polygon closetDoorFront = new Polygon(new Point(-225 + j * tileWidth, 0, 100), new Point(-225 + tileWidth + j * tileWidth - gap, 0, 100), new Point(-225 + +tileWidth + j * tileWidth - gap, tileHeight, 100), new Point(-225 + j * tileWidth, tileHeight, 100));
             closetDoorFront.setEmission(new Color(127, 255, 0)).setMaterial(new Material().setKs(0).setKd(0.5));
             scene.geometries.add(closetDoorFront);
-            Polygon closetDoorLeft = new Polygon(new Point(-225 + j * tileWidth, 0, 100), new Point(-225 + j * tileWidth, 0, 0), new Point(-225 + j * tileWidth, tileHeight, 0), new Point(-225 + j * tileWidth, tileHeight, 100));
+            Polygon closetDoorLeft = new Polygon(new Point(-225 + j * tileWidth, 0, 100), new Point(-225 + j * tileWidth, 0, fixLeft), new Point(-225 + j * tileWidth, tileHeight, fixLeft), new Point(-225 + j * tileWidth, tileHeight, 100));
             closetDoorLeft.setEmission(new Color(100, 100, 100)).setMaterial(new Material().setKs(0).setKd(0.5));
             scene.geometries.add(closetDoorLeft);
-            Polygon closetDoorRight = new Polygon(new Point(-225 + tileWidth + j * tileWidth - gap, 0, 100), new Point(-225 + tileWidth + j * tileWidth - gap, 0, 0), new Point(-225 + tileWidth + j * tileWidth - gap, tileHeight, 0), new Point(-225 + tileWidth + j * tileWidth - gap, tileHeight, 100));
+
+            Polygon closetDoorRight = new Polygon(new Point(-225 + tileWidth + j * tileWidth - gap, 0, 100), new Point(-225 + tileWidth + j * tileWidth - gap, 0, fixRight), new Point(-225 + tileWidth + j * tileWidth - gap, tileHeight, fixRight), new Point(-225 + tileWidth + j * tileWidth - gap, tileHeight, 100));
             closetDoorRight.setEmission(new Color(100, 100, 100)).setMaterial(new Material().setKs(0).setKd(0.5));
             scene.geometries.add(closetDoorRight);
+
         }
+        //endregion
 
-       /* Polygon closetBackground = new Polygon(new Point(500, 200, 50), new Point(-225, 200, 50), new Point(-225, 0, 50), new Point(500, 0, 50));
-        closetBackground.setEmission(new Color(0, 0, 0)).setMaterial(new Material().setKs(0).setKd(0.5));
-        scene.geometries.add(closetBackground);*/
-
-        //floor background
-        Polygon floorBackground = new Polygon(new Point(-500, -2, 0), new Point(-500, -2, 250), new Point(500, -2, 250), new Point(500, -2, 0));
+        //region floor
+        // background
+        Polygon floorBackground = new Polygon(new Point(-500, -2, 0), new Point(-500, -2, 300), new Point(500, -2, 300), new Point(500, -2, 0));
         floorBackground.setMaterial(new Material());
         floorBackground.setEmission(new Color(122, 122, 122));
         scene.geometries.add(floorBackground);
@@ -128,7 +213,7 @@ public class bathroom {
         tileHeight = 50;
         tileWidth = 50;
         gap = 2;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 20; j++) {
                 Polygon polygon = new Polygon(new Point(-500 + j * tileWidth, 0, i * tileHeight), new Point(-500 + tileWidth + j * tileWidth - gap, 0, i * tileHeight), new Point(-500 + tileWidth + j * tileWidth - gap, 0, tileHeight + i * tileHeight - gap), new Point(-500 + j * tileWidth, 0, tileHeight + i * tileHeight - gap));
                 polygon.setEmission(new Color(143, 188, 143)).setMaterial(new Material().setKs(0).setKd(1));
@@ -136,9 +221,11 @@ public class bathroom {
                 //new Color(143,188,143)
             }
         }
+        //endregion
 
-        //right wall back
-        Polygon rightWall = new Polygon(new Point(501, 0, 0), new Point(501, 0, 250), new Point(501, 500, 250), new Point(501, 500, 0));
+        //region right wall
+        // back
+        Polygon rightWall = new Polygon(new Point(501, 0, 0), new Point(501, 0, 300), new Point(501, 500, 300), new Point(501, 500, 0));
         rightWall.setMaterial(new Material());
         rightWall.setEmission(new Color(122, 122, 122));
         scene.geometries.add(rightWall);
@@ -159,21 +246,50 @@ public class bathroom {
         tileHeight = 50;
         tileWidth = 20;
         gap = 1;
-        for (int j = 0; j < 13; j++) {
-            Polygon polygon = new Polygon(new Point(499, 225, j * tileWidth), new Point(499, 225, tileWidth + j * tileWidth - gap), new Point(499, 225 + tileHeight, tileWidth + j * tileWidth - gap), new Point(499, 225 + tileHeight, j * tileWidth));
-            polygon.setEmission(colorPallete[j % 5]).setMaterial(new Material().setKs(0).setKd(0.5));
+        for (int j = 0; j < 15; j++) {
+            Polygon polygon = new Polygon(new Point(499.9, 225, j * tileWidth), new Point(499.9, 225, tileWidth + j * tileWidth - gap), new Point(499.9, 225 + tileHeight, tileWidth + j * tileWidth - gap), new Point(499.9, 225 + tileHeight, j * tileWidth));
+            polygon.setEmission(colorPallet[j % 5]).setMaterial(new Material().setKs(0).setKd(0.5));
             scene.geometries.add(polygon);
         }
+        //endregion
 
-        //camera.moveCamera(new Point(-1000, 250,500 ),new Point(0, 250,50 ));
-        //taking picture
-        ImageWriter imageWriter = new ImageWriter("zzzzz", 1000, 1000);
+        //region taking picture
+        //front always first!
+        renderFront(scene,camera);
+        //From left
+        renderSide(scene,camera);
+        //From top
+        renderTop(scene,camera);
+        //endregion
+    }
+
+    //region rendering positions
+    private void renderFront(Scene scene,Camera camera){
+        camera.moveCamera(new Point(0, 300, 1000),new Point(0,250,50));
+        ImageWriter imageWriter = new ImageWriter("zFront", 1000, 1000);
         camera.setImageWriter(imageWriter) //
                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage(); //
         camera.writeToImage();
-
-
     }
+
+    private void renderSide(Scene scene,Camera camera){
+        camera.moveCamera(new Point(-1000, 250,1000 ),new Point(0, 250,50 ));
+        ImageWriter imageWriter = new ImageWriter("zSide", 1000, 1000);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage(); //
+        camera.writeToImage();
+    }
+
+    private void renderTop(Scene scene,Camera camera){
+        camera.moveCamera(new Point(0, 1000, 500), new Point(0, 250, 50));
+        ImageWriter imageWriter = new ImageWriter("zTop", 1000, 1000);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage(); //
+        camera.writeToImage();
+    }
+    //endregion
 
 }
